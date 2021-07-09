cb4b5838dc0f69848881b30d75044c9a8ec5d977
Charlou <charloua@posbang.com>
2021-07-10 00:55:06 +0800

    facebook login

--- lib/src/cubits/auth_cubit.dart
+++ lib/src/cubits/auth_cubit.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:eigital_handson/src/utils/index.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthCubitState {
@@ -16,14 +17,14 @@ class AuthCubitState {
   Map<String, dynamic> toMap() {
     return {
       'name': name,
-      'loading': loading,
+      'loading': false,
     };
   }
 
   factory AuthCubitState.fromMap(Map<String, dynamic> map) {
     return AuthCubitState(
       map['name'],
-      map['loading'],
+      false,
     );
   }
 
@@ -53,21 +54,22 @@ class AuthCubit extends Cubit<AuthCubitState> {
   }
 
   void loginFacebook() async {
-    updateState(state.copyWith(loading: true));
-    // TODO: login with facebook
+    try {
+      updateState(state.copyWith(loading: true));
+      final result = await FacebookAuth.instance.login();
+      final facebookAuthCredential = FacebookAuthProvider.credential(result.accessToken?.token ?? '');
+      final cred = await FirebaseAuth.instance.signInWithCredential(facebookAuthCredential);
+      updateState(state.copyWith(name: cred.user?.displayName, loading: false));
+    } catch (e) {
+      updateState(state.copyWith(loading: false));
+    }
   }
 
   void loginGoogle() async {
     updateState(state.copyWith(loading: true));
     final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
     if (googleUser != null) {
-      final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
-      final credential = GoogleAuthProvider.credential(
-        accessToken: googleAuth.accessToken,
-        idToken: googleAuth.idToken,
-      );
-      final cred = await FirebaseAuth.instance.signInWithCredential(credential);
-      updateState(state.copyWith(name: cred.user?.displayName));
+      updateState(state.copyWith(name: googleUser.displayName));
     }
     updateState(state.copyWith(loading: false));
   }
